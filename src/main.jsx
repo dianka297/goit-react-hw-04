66aa00329aedcb901e7d72f460504e2ae40583c8
dianka297 <dianamakharadze96@gmail.com>
2025-01-10 16:28:10 +0400

    add task done

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import "modern-normalize";
+import "modern-normolize";
 import App from "./components/App";
 // import Modal from "react-modal";
 
